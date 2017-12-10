2bbbb5c99e6222c6354e8b4932be8d900bbd9968
Reid Klein <Reid Klein>
2017-12-09 21:38:36 -0600

    Add project files.

---
+++ OliviaDressageWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OliviaDressageWeb.MvcApplication" Language="C#" %>
---
+++ OliviaDressageWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OliviaDressageWeb.Startup))]
+namespace OliviaDressageWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
